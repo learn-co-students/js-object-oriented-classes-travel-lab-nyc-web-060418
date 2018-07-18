b1c4c4ef372c70b501355614617d854b421beb6d
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-07-18 11:44:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
 
+  constructor(name, startDateString) {
+    this.name = name;
+    this.startDate = new Date(startDateString);
+  }
+
+  yearsExperienceFromBeginningOf(year) {return year - this.startDate.getFullYear()}
+
+}
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let verticalDistance = 0;
+    let horizontalDistance = 0;
+    verticalDistance = Math.abs(parseInt(this.beginningLocation.vertical) - parseInt(this.endingLocation.vertical))
+    let startAveIndex = eastWest.indexOf(this.beginningLocation.horizontal)
+    let endAveIndex = eastWest.indexOf(this.endingLocation.horizontal)
+    horizontalDistance = Math.abs(startAveIndex - endAveIndex)
+    return verticalDistance + horizontalDistance
+  }
+
+  estimatedTime(peakHoursBool) {
+    if (peakHoursBool) {
+      return  (this.blocksTravelled() / 2)
+    }
+    else {
+      return (this.blocksTravelled() / 3)
+    }
+  }
+
+}
